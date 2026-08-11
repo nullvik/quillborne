a8fb6cc064809882619ba13851413f317066ff75
mikanullvik <249939325+mikanullvik@users.noreply.github.com>
2026-08-11 15:09:11 +0200

    Add NavigationBar

---
+++ ViewModels/NavigationBarViewModel.cs
@@ -0,0 +1,17 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace quillborne.ViewModels;
+
+public partial class NavigationBarViewModel : ViewModelBase
+{
+    [RelayCommand]
+    private void OpenSettings()
+    {
+    }
+
+    [RelayCommand]
+    private void FileOperations()
+    {
+    }
+}
---
+++ Views/NavigationBar.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace quillborne.Views;
+
+public partial class NavigationBar : UserControl
+{
+    public NavigationBar()
+    {
+        InitializeComponent();
+    }
+}
