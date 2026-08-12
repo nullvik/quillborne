89d5f73b2c1ef89b2155a956fefbf0c1968f244d
mikanullvik <249939325+mikanullvik@users.noreply.github.com>
2026-08-12 14:16:44 +0200

    Add NavigationBar command base

--- ViewModels/NavigationBarViewModel.cs
+++ ViewModels/NavigationBarViewModel.cs
@@ -5,4 +5,104 @@ namespace quillborne.ViewModels;
 
 public partial class NavigationBarViewModel : ViewModelBase
 {
+    // File Commands
+    [RelayCommand]
+    private void NewFile()
+    {
+
+    }
+
+    [RelayCommand]
+    private void OpenFile()
+    {
+
+    }
+
+    [RelayCommand]
+    private void SaveFile()
+    {
+
+    }
+
+    [RelayCommand]
+    private void ExportFile()
+    {
+
+    }
+
+    // Edit Commands
+    [RelayCommand]
+    private void UndoEdit()
+    {
+
+    }
+
+    [RelayCommand]
+    private void RedoEdit()
+    {
+
+    }
+
+    [RelayCommand]
+    private void CutEdit()
+    {
+
+    }
+
+    [RelayCommand]
+    private void CopyEdit()
+    {
+
+    }
+
+    [RelayCommand]
+    private void PasteEdit()
+    {
+
+    }
+
+    // View Commands
+    [RelayCommand]
+    private void SidebarView()
+    {
+
+    }
+
+    [RelayCommand]
+    private void OutlineView()
+    {
+
+    }
+
+    [RelayCommand]
+    private void WordCountView()
+    {
+
+    }
+
+    // Search Commands
+    [RelayCommand]
+    private void FindSearch()
+    {
+
+    }
+
+    [RelayCommand]
+    private void ReplaceSearch()
+    {
+
+    }
+
+    // Options Commands
+    [RelayCommand]
+    private void SettingsOptions()
+    {
+
+    }
+
+    [RelayCommand]
+    private void AboutOptions()
+    {
+
+    }
 }
