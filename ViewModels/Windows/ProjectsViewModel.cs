0d660b2fad23b86a328057ae96872c3573c7127b
nullvik <249939325+mikanullvik@users.noreply.github.com>
2026-08-20 22:46:00 +0200

    Minor changes

--- ViewModels/Windows/ProjectsViewModel.cs
+++ ViewModels/Windows/ProjectsViewModel.cs
@@ -1,8 +1,24 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace quillborne.ViewModels.Windows;
 
 public partial class ProjectsViewModel : ObservableObject
 {
+    [RelayCommand]
+    private void NewProject()
+    {
+        // Spawn input for project name
+        // Check name against rules
+        // Create project with name
+        // Open project in main window
+    }
 
+    [RelayCommand]
+    private void OpenProject()
+    {
+        // Open project in main window
+    }
 }
